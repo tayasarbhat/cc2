3ed19c550320c01241d690f4d28d5438cc91cbb8
tayasarbhat <tayasarbhat@gmail.com>
2024-11-08 11:22:49 +0530

    Add files via upload

---
+++ vite.config.ts
@@ -0,0 +1,7 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
+export default defineConfig({
+  base: '/ACS/', // Replace 'your-repo-name' with the actual repository name
+  plugins: [react()],
+});
